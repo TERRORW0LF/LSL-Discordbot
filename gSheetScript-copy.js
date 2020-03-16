39979d98beed7cc1bba96408289f9e80eb3ef509
TERRORW0LF <61637480+TERRORW0LF@users.noreply.github.com>
2020-03-17 00:00:40 +0100

    Create gSheetScript-copy.js

---
+++ gSheetScript-copy.js
@@ -0,0 +1,28 @@
+function formSubmit(e) {
+  var submitVals = []
+  const values = e.values;
+  const submit = values[0].split('/')[2].split(' ').slice(1, 2);
+  submitVals.push(values[0].replace(' '+submit, ''));
+  values.slice(1).map(function (str) {submitVals.push(str)});
+  const postdata = {
+    "method": "post",
+    "headers": {
+      "contentType": "application/json"
+    },
+    "payload": {
+      "user": submitVals[1],
+      "map": submitVals[4],
+      "season": "season3",
+      "mode": submitVals[5],
+      "time": submitVals[2],
+      "link": submitVals[3],
+      "date": submitVals[0]
+    }
+  };
+  UrlFetchApp.fetch('https://lsl-discordbot.herokuapp.com/submit?auth=YouShallNotPass', postdata);
+}
+
+function rowDelete(e) {
+  if (e.changeType !== "REMOVE_ROW") return;
+  UrlFetchApp.fetch('https://lsl-discordbot.herokuapp.com/delete?auth=YouShallNotPass');
+}
