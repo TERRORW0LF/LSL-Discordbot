8435980f1a38eac49c8678cf93dd3c4d229e496b
TERRORW0LF <61637480+TERRORW0LF@users.noreply.github.com>
2020-03-18 12:53:42 +0100

    Update getUser.js

--- Util/getUser.js
+++ Util/getUser.js
@@ -4,7 +4,6 @@ async function getUser(guild, user) {
     const guildMembers = await guild.fetchMembers();
     const members = await guildMembers.members;
     const mention = await members.find(u => u.user.tag === user);
-    if (!mention) console.log(user);
     if (!mention) return user.split('#')[0]; 
     else return mention;
 }
