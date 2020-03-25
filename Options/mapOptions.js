1fadcffacacfb54d97f7cf7e71c448e5ba949270
TERRORW0LF <61637480+TERRORW0LF@users.noreply.github.com>
2020-03-25 15:53:46 +0100

    Update mapOptions.js

--- Options/mapOptions.js
+++ Options/mapOptions.js
@@ -7,15 +7,17 @@ function getMapOptions(map) {
     'Route 66','Gibraltar','Blizzard World','Eichenwalde','Hollywood',"King's Row",'Numbani', 'Busan Sanctuary','Busan MEKA Base',
     'Busan Downtown','Ilios Well','Ilios Ruins','Ilios Lighthouse','Lijiang Night Market','Lijiang Garden','Lijiang Control Center',
     'Nepal Village','Nepal Sanctum','Nepal Shrine','Oasis City Center','Oasis University','Oasis Gardens'];
-
+    const mapOptionsLow = mapOptions.map(str => str.toLowerCase());
     var opts = [];
     var redMapOpts = mapOptions;
+    var redMapOptsLow = mapOptionsLow;
     for (i = 0; i < 5; i++) {
-        const d = strcomp.findBestMatch(map, redMapOpts);
+        const d = strcomp.findBestMatch(map.toLowerCase(), redMapOptsLow);
         if (d.bestMatch.rating < 0.35) return opts;
-        opts.push(d.bestMatch.target);
+        opts.push(redMapOpts[d.bestMatchIndex]);
         if (d.bestMatch.rating > 0.7) return opts;
         redMapOpts.splice(d.bestMatchIndex, 1);
+        redMapOptsLow.splice(d.bestMatchIndex, 1);
     }
     return opts;
 }
