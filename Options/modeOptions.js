2f933ca20a27906fd69bfb6c1bb4fc39c9f9f2bc
TERRORW0LF <61637480+TERRORW0LF@users.noreply.github.com>
2020-03-25 16:05:45 +0100

    Update modeOptions.js

--- Options/modeOptions.js
+++ Options/modeOptions.js
@@ -3,7 +3,7 @@ const strcomp = require('string-similarity');
 module.exports = getModeOptions;
 
 function getModeOptions(mode) {
-    const d = strcomp.findBestMatch(mode, ['Gravspeed', 'Standard']);
+    const d = strcomp.findBestMatch(mode.toLowerCase(), ['gravspeed', 'standard']);
     if (d.bestMatch.rating < 0.35) return;
     return d.bestMatch.target;
 }
