4ce8a4f12fe517f73473aa7bf41f1d71f116d62a
TERRORW0LF <61637480+TERRORW0LF@users.noreply.github.com>
2020-05-28 14:57:20 +0200

    Update seasonOptions.js

--- Options/seasonOptions.js
+++ Options/seasonOptions.js
@@ -5,6 +5,7 @@ function getSeasonOptions(season) {
         case '1': return 'season1';
         case '2': return 'season2';
         case '3': return 'season3';
+        case '4': return 'season4';
         default: return;
     }
 }
