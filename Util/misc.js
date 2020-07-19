26bcbadcd6955221f8f3a414d9da5e62561d14a5
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-19 17:08:41 +0200

    add 'getAllSubmits', remove caching.

--- Util/misc.js
+++ Util/misc.js
@@ -1,4 +1,7 @@
-module.exports = clearMsg
+const getGoogleAuth = require('../google-auth');
+const { google } = require('googleapis');
+
+module.exports = clearMsg, getAllSubmits;
 
 async function clearMsg(botMsg, msg) {
     for (let reaction in msg.reactions.cache) {
@@ -7,4 +10,27 @@ async function clearMsg(botMsg, msg) {
     for (let reaction in botMsg.reactions.cache) {
         if (reaction.me) reaction.remove();
     }
+}
+
+async function getAllSubmits(sheet, sheetrange) {
+    const token = getGoogleAuth();
+    const client = google.sheets('v4');
+    const response = (await client.spreadsheets.values.get({
+        auth: token,
+        spreadsheetId: sheet,
+        range: sheetrange,
+        majorDimension: 'ROWS'
+    })).data;
+    let output = [];
+    for (row of response) {
+        output.push({
+            date: row[0],
+            name: row[1],
+            time: row[2],
+            proof: row[3],
+            stage: row[4],
+            category: row[5]
+        });
+    }
+    return output;
 }
