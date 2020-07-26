f60f64ca743f3f3f0f02f14392694c161d811b22
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-26 23:32:57 +0200

    add google sheets values | add roleupdate options.

--- Util/roleUpdate.js
+++ Util/roleUpdate.js
@@ -8,9 +8,23 @@ module.exports = roleUpdate;
 
 async function roleUpdate(guild, season) {
     try {
+        if (serverCfg[guild.id].roleOptions.role === "none") return;
         const roles = serverCfg[guild.id].roles,
-              allUsers = guild.members.fetch(),
-              requiredUsers = getPoints(process.env[`gSheetS${season}`], 'Point Sheet!')
+              allUsers = guild.members.fetch();
+        switch (serverCfg[guild.id].roleOptions.role) {
+            case "highestTotal": // One role --> highest total points out of all seasons.
+                break;
+            case "highestCategory": // One role --> highest category points out of all seasons.
+                break;
+            case "highestSeasonTotal": // One role per season --> highest total points of each season.
+                break;
+            case "highestSeasonCategory": // One role per season --> highest category points of each season.
+                break;
+            case "allSeasonCategory": // One role per season per category.
+                break;
+            default:
+                break;
+        }
         console.log('\nRoleUpdate done\n');
     } catch (err) {
         console.log('An error occured in roleUpdate: '+err.message);
