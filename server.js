e3809614c8d49e6322ee5f53ce0135aebd88f504
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-29 00:55:53 +0200

    welp, back to the old structure.

--- server.js
+++ server.js
@@ -29,43 +29,43 @@ process.on('uncaughtException', err => {
 const client = new Discord.Client();
 client.login(process.env.discordTOKEN);
 
+// Discord events
+client.on('ready', () => {
+    console.log('Discord bot up and running!');
+});
+
+client.on('message', msg => {
+    const prefix = serverCfg[msg.guild.id].prefix;
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    if (!serverCfg[msg.guild.id].channels.commands.length || !serverCfg[msg.guild.id].channels.commands.some(value => value === msg.channel.id)) {
+        msg.channel.send('please post commands in the designated channels.');
+        return;
+    }
+    let answered = false;
+    for (let command of commands.commandList) {
+        let pattern = new RegExp(command.regex, "i");
+        if (pattern.test(msg.content.replace(prefix, '').trim())) {
+            const permission = serverCfg[msg.guild.id].permissions[command.group];
+            if (permission) {
+                let hasPermission = false;
+                for (role of permission) {
+                    if (msg.member.roles.cache.has(role)) hasPermission = true;
+                }
+                if (!hasPermission) break;
+            }
+            const run = require(`./${command.path}`);
+            run(msg, client, pattern.exec(msg.content.replace(prefix, '').trim()));
+            answered = true;
+        }
+    }
+    if (!answered) msg.channel.send("❌ No matching command found.")
+});
+
 // Process app / Webhook listener
 const P = process.env.PORT ||  3000;
 
 (async function init () {
     try {
-        // Discord events
-        client.on('ready', () => {
-            console.log('Discord bot up and running!');
-        });
-
-        client.on('message', msg => {
-            const prefix = serverCfg[msg.guild.id].prefix;
-            if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-            if (!serverCfg[msg.guild.id].channels.commands.length || !serverCfg[msg.guild.id].channels.commands.some(value => value === msg.channel.id)) {
-                msg.channel.send('please post commands in the designated channels.');
-                return;
-            }
-            let answered = false;
-            for (let command of commands.commandList) {
-                let pattern = new RegExp(command.regex, "i");
-                if (pattern.test(msg.content.replace(prefix, '').trim())) {
-                    const permission = serverCfg[msg.guild.id].permissions[command.group];
-                    if (permission) {
-                        let hasPermission = false;
-                        for (role of permission) {
-                            if (msg.member.roles.cache.has(role)) hasPermission = true;
-                        }
-                        if (!hasPermission) break;
-                    }
-                    const run = require(`./${command.path}`);
-                    run(msg, client, pattern.exec(msg.content.replace(prefix, '').trim()));
-                    answered = true;
-                }
-            }
-            if (!answered) msg.channel.send("❌ No matching command found.")
-        });
-
         // Start Webhooks listener.
         app.use(express.json());
         await setGoogleAuth();
