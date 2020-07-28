10038c147e46cf83f619ba4f49be30afb258269e
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-29 00:51:15 +0200

    Fix late start up

--- server.js
+++ server.js
@@ -34,40 +34,36 @@ const P = process.env.PORT ||  3000;
 
 (async function init () {
     try {
+        // Discord events
         client.on('ready', () => {
             console.log('Discord bot up and running!');
-        
-            // Discord events
-            client.on('message', msg => {
-                const prefix = serverCfg[msg.guild.id].prefix;
-                if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-                if (!serverCfg[msg.guild.id].channels.commands.length || !serverCfg[msg.guild.id].channels.commands.some(value => value === msg.channel.id)) {
-                    msg.channel.send('please post commands in the designated channels.');
-                    return;
-                }
-                let answered = false;
-                for (let command of commands.commandList) {
-                    let pattern = new RegExp(command.regex, "i");
-                    if (pattern.test(msg.content.replace(prefix, '').trim())) {
-                        const permission = serverCfg[msg.guild.id].permissions[command.group];
-                        if (permission) {
-                            let hasPermission = false;
-                            for (role of permission) {
-                                if (msg.member.roles.cache.has(role)) hasPermission = true;
-                            }
-                            if (!hasPermission) break;
+        });
+
+        client.on('message', msg => {
+            const prefix = serverCfg[msg.guild.id].prefix;
+            if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+            if (!serverCfg[msg.guild.id].channels.commands.length || !serverCfg[msg.guild.id].channels.commands.some(value => value === msg.channel.id)) {
+                msg.channel.send('please post commands in the designated channels.');
+                return;
+            }
+            let answered = false;
+            for (let command of commands.commandList) {
+                let pattern = new RegExp(command.regex, "i");
+                if (pattern.test(msg.content.replace(prefix, '').trim())) {
+                    const permission = serverCfg[msg.guild.id].permissions[command.group];
+                    if (permission) {
+                        let hasPermission = false;
+                        for (role of permission) {
+                            if (msg.member.roles.cache.has(role)) hasPermission = true;
                         }
-                        const run = require(`./${command.path}`);
-                        run(msg, client, pattern.exec(msg.content.replace(prefix, '').trim()));
-                        answered = true;
+                        if (!hasPermission) break;
                     }
+                    const run = require(`./${command.path}`);
+                    run(msg, client, pattern.exec(msg.content.replace(prefix, '').trim()));
+                    answered = true;
                 }
-                if (!answered) msg.channel.send("❌ No matching command found.")
-            });
-        
-            // Initialize webhooks handling submits and deletes to google sheets.
-            app.post('/submit', newSubmit(client));
-            app.post('/delete', newDelete(client));
+            }
+            if (!answered) msg.channel.send("❌ No matching command found.")
         });
 
         // Start Webhooks listener.
@@ -77,6 +73,9 @@ const P = process.env.PORT ||  3000;
 
         app.listen(P, () => console.log('app running on PORT: ', P));
 
+        // Initialize webhooks handling submits and deletes to google sheets.
+        app.post('/submit', newSubmit(client));
+        app.post('/delete', newDelete(client));
         // Webhook to keep the bot awake (Fuck you Heroku).
         app.get('/ping', (req, res) => {
             if(req.query.auth !== process.env.herokuAUTH) {
