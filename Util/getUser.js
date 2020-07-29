612e3fd02a003b6fc8a9b77d997adb6fb9f95d1d
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-29 21:52:10 +0200

    limits

--- Util/getUser.js
+++ Util/getUser.js
@@ -1,7 +1,7 @@
 module.exports = getUser;
 
 async function getUser(guild, user) {
-    const similarUsers = await guild.members.fetch({query: user.split('#')[0]});
+    const similarUsers = await guild.members.fetch({query: user.split('#')[0], limit: 5});
     console.log(similarUsers);
     if (!similarUsers) return user.split('#')[0];
     const mention = similarUsers.find(member => member.user.tag === user);
