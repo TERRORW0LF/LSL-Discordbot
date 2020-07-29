7c8462bdcaf8eb67435ff2e7cdd11c59b5972bf1
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-29 15:55:49 +0200

    ok this should fix it

--- options.js
+++ options.js
@@ -9,7 +9,7 @@ function getSeasonOptions(season, id) {
 
 
 function getModeOptions(mode, id) {
-    const modes = serverCfg[id].categories,
+    const modes = serverCfg[id].categories.map(value => value),
           modesLow = modes.map(category => category.toLowerCase());
     let opts = [];
     for (i = 0; i < 5 && modes.length; i++) {
@@ -19,14 +19,13 @@ function getModeOptions(mode, id) {
         if (d.bestMatch.rating > 0.7) return opts;
         modes.splice(d.bestMatchIndex, 1);
         modesLow.splice(d.bestMatchIndex, 1);
-        console.log(serverCfg[id].categories);
     }
     return opts;
 }
 
 
 function getMapOptions(stage, id) {
-    const maps = serverCfg[id].stages,
+    const maps = serverCfg[id].stages.map(value => value),
           mapsLow = maps.map(str => str.toLowerCase());
     let opts = [];
     for (i = 0; i < 5 && maps.length; i++) {
@@ -36,7 +35,6 @@ function getMapOptions(stage, id) {
         if (d.bestMatch.rating > 0.7) return opts;
         maps.splice(d.bestMatchIndex, 1);
         mapsLow.splice(d.bestMatchIndex, 1);
-        console.log(serverCfg[id].stages);
     }
     return opts;
 }
