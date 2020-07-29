cc306bf261f2122f3ff71679f9a6560415dd2218
Finn Brandt <finn2003minicooper@gmail.com>
2020-07-29 21:59:32 +0200

    remove logging

--- Util/getUser.js
+++ Util/getUser.js
@@ -2,7 +2,6 @@ module.exports = getUser;
 
 async function getUser(guild, user) {
     const similarUsers = await guild.members.fetch({query: user.split('#')[0], limit: 100});
-    console.log(similarUsers);
     if (!similarUsers) return user.split('#')[0];
     const mention = similarUsers.find(member => member.user.tag === user);
     return mention ? mention : user.split('#')[0];
