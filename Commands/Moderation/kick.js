d8d4409451bc14034e4fb9cbd9c05189412502c0
TERRORWOLF <finn2003minicooper@gmail.com>
2020-09-19 01:57:49 +0200

    add moderation commands.

---
+++ Commands/Moderation/kick.js
@@ -0,0 +1,26 @@
+const { clearMsg } = require("../../Util/misc");
+
+module.exports = run;
+
+async function run(msg, clinet, regexGroups) {
+    await msg.react('💬');
+    const botMsg = await msg.channel.send('💬 Searching user data, please hold on.');
+    try {
+        if (msg.mentions.members.length > 1) {
+            clearMsg(botMsg, msg);
+            msg.react('❌');
+            botMsg.edit('❌ Please only mention one user.');
+            return;
+        }
+        msg.mentions.members.first().kick(regexGroups[3]);
+        clearMsg(botMsg, msg);
+        msg.react('✅');
+        botMsg.edit(`✅ Successfully kicked ${msg.mentions.members.first().tag}.`);
+    } catch (err) {
+        clearMsg(botMsg, msg);
+        msg.react('❌');
+        botMsg.edit('❌ An error occurred while handling your command.');
+        console.log('Error in kick: ' + err.message);
+        console.log(err.stack);
+    }
+}
---
+++ Commands/Moderation/mute.js
@@ -0,0 +1,5 @@
+module.exports = run;
+
+async function run(msg, clinet, regexGroups) {
+    
+}
