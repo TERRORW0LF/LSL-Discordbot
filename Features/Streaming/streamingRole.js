592881da78598ec7f8f0f9ba98ce9a98659e0473
Finn Brandt <finn2003minicooper@gmail.com>
2020-10-14 18:52:31 +0200

    only execute when member is manageable

--- Features/Streaming/streamingRole.js
+++ Features/Streaming/streamingRole.js
@@ -5,6 +5,7 @@ module.exports = run;
 
 async function run(oldPresence, newPresence) {
     try {
+        if (!newPresence.member.manageable) return;
         const streamingRole = serverCfg[newPresence.guild.id].features.streaming.role;
         if ((!oldPresence || oldPresence.activities.some(activity => activity.type === 'STREAMING')) && !newPresence.activities.some(activity => activity.type === 'STREAMING')) {
             newPresence.member.roles.remove(streamingRole).catch(() => {
