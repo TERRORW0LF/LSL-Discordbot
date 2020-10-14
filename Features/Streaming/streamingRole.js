8e5600c85402c4487f2b70c58215c6dcd3eee0e6
Finn Brandt <finn2003minicooper@gmail.com>
2020-10-14 19:20:37 +0200

    typo

--- Features/Streaming/streamingRole.js
+++ Features/Streaming/streamingRole.js
@@ -19,7 +19,7 @@ async function run(oldPresence, newPresence) {
                     break;
                 }
             }
-            if (!hasrole) return;
+            if (!hasRole) return;
         }
         const streamingRole = serverCfg[newPresence.guild.id].features.streaming.role;
         if ((!oldPresence || oldPresence.activities.some(activity => activity.type === 'STREAMING')) && !newPresence.activities.some(activity => activity.type === 'STREAMING')) {
