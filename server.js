0861c8ba378753b712f36014d1f8f5bf1a8a4a83
Finn Brandt <finn2003minicooper@gmail.com>
2020-10-15 21:54:55 +0200

    correct serverCfg handling

--- server.js
+++ server.js
@@ -91,9 +91,8 @@ client.on('messageReactionRemove', async (reaction, user) => {
 });
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
-    let featureCfg;
-    if (serverCfg[newPresence.guild.id]) featureCfg = serverCfg[newPresence.guild.id].features;
-    else featureCfg = serverCfg["574522788967088128"].features;
+    if (!serverCfg[newPresence.guild.id]) return;
+    const featureCfg = serverCfg[newPresence.guild.id].features;
     if (featureCfg.streaming.enabled) {
         const run = require('./Features/Streaming/streamingRole');
         run(oldPresence, newPresence);
