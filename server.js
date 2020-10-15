f1fc2d3dafa09cdfa4ec21721c274c728934ff2e
Finn Brandt <finn2003minicooper@gmail.com>
2020-10-15 16:40:30 +0200

    reduce pingSelf time.

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ function pingSelf () {
     if (!process.env.PORT) 
         return;
     setInterval(async () => {
-        axios.get(`https://lsl-discordbot-v12.herokuapp.com/ping?auth=${process.env.herokuAUTH}`).catch(err => { return; });
-        axios.get(`https://discord-lsl.herokuapp.com/ping?auth=${process.env.herokuAUTH}`).catch(err => { return; });
-    }, 1700000);
+        axios.get(`https://lsl-discordbot-v12.herokuapp.com/ping?auth=${process.env.herokuAUTH}`).catch(err => { });
+        axios.get(`https://discord-lsl.herokuapp.com/ping?auth=${process.env.herokuAUTH}`).catch(err => { });
+    }, 1200000);
 }
