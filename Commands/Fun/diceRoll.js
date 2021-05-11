ecf506a6897deed59eb01af8ecd6549f94359de9
TERRORWOLF <finn2003minicooper@gmail.com>
2021-05-11 22:26:25 +0200

    prevent process running out of memory.

--- Commands/Fun/diceRoll.js
+++ Commands/Fun/diceRoll.js
@@ -10,9 +10,12 @@ async function run(msg, client, regexGroups) {
         if (regexGroups[2] === '0') return msg.channel.send(createEmbed('Cannot roll 0 dice.', 'Error', msg.guild.id));
         if (!regexGroups[2]) regexGroups[2] = "1";
         let numbers = `${Math.round(Math.random()*parseInt(regexGroups[3]))}`;
-        for (let i=1; i<regexGroups[2]; i++)
+        for (let i=1; i<regexGroups[2]; i++) {
             numbers += `, ${Math.round(Math.random()*parseInt(regexGroups[3]))}`
-        msg.channel.send(createEmbed(`**${msg.member.nickname || msg.author.username}**  Rolled ${regexGroups[2]} ${regexGroups[3]} sided dice and got: **${numbers}**`, 'Success', msg.guild.id));
+            if (numbers.length + regexGroups[2].length + regexGroups[3].length + 70 > 2000)
+                return msg.channel.send(createEmbed(`Failed to roll. Message is longer than 2000 characters.`, 'Error', msg.guild.id));
+        }
+        msg.channel.send(createEmbed(`**${msg.member.displayName}** Rolled ${regexGroups[2]} ${regexGroups[3]} sided dice and got: **${numbers}**`, 'Success', msg.guild.id));
     } catch (err) {
         msg.channel.send(createEmbed('An error occurred while handling your command. Informing staff.', 'Error', msg.guild.id));
         console.log('An error occured in dadJoke: '+err.message);
