c11dfd65a41baa9ab76f97f179359fb2d0134d22
Finn Brandt <finn2003minicooper@gmail.com>
2021-08-08 13:22:13 +0200

    clear branch

--- Util/getUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = getUser;
-
-async function getUser(guild, user) {
-    const guildMembers = await guild.fetchMembers();
-    const members = await guildMembers.members;
-    const mention = await members.find(u => u.user.tag === user);
-    if (!mention) return user.split('#')[0]; 
-    else return mention;
-}
