26e72b54361c64620263e3f27525bbb7bad1091d
Finn Brandt <finn2003minicooper@gmail.com>
2021-08-09 19:19:21 +0200

    initial setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { discordToken, port } from './config/config';
+import { discordToken, port } from './config/config.js';
 import https from 'https';
 import express from 'express';
 import { Client, Intents } from 'discord.js';
@@ -12,16 +12,20 @@ const app = express();
 
 app.get('/ping', (req, res) => {
     console.log('\nping\n');
-    res.statusCode(200);
+    res.sendStatus(200);
 });
 app.get('/submit', (req, res) => { 
-    if (!client.isReady()) res.statusCode(404);
+    if (!client.isReady()) res.sendStatus(404);
 });
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.once('ready', () => { 
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    
 });
 
 app.listen(port, () => {
