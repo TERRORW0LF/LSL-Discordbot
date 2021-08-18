b55e8582061ba492ce1dabef39ca4975609872b6
TERRORWOLF <finn2003minicooper@gmail.com>
2021-08-18 18:38:44 +0200

    implement command register

--- src/commandRegister.js
+++ src/commandRegister.js
@@ -1,2 +1,26 @@
 'use strict'
 
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types';
+import fs from 'fs';
+import { discordToken, clientId } from './config/config';
+
+let commands = [];
+const commandFiles = fs.readdirSync('./commands/top level').filter(file => file.endsWith('.js'));
+
+for (const file in commandFiles) {
+    import command from `./commands/top level/${file}`;
+    commands.push(command.data.toJson());
+}
+
+const rest = new REST({ version: 9 }).setToken(discordToken);
+
+(async () => {
+    try {
+        console.log('Started refreshing application commands.');
+        await rest.put(Routes.applicationCommands(clientId), { body: commands });
+        console.log('Successfully reloaded application commands.');
+    } catch (err) {
+        console.error(err);
+    }
+})();
--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,5 @@
-export const discordToken = process.env.DISCORD_TOKEN, googleToken = process.env.GOOGLE_TOKEN, googleEmail = process.env.GOOGLE_EMAIL, port = process.env.PORT;
+export const discordToken = process.env.DISCORD_TOKEN,
+    clientId = process.env.DISCORD_APPLICATION_ID,
+    googleKey = process.env.GOOGLE_TOKEN, 
+    googleEmail = process.env.GOOGLE_EMAIL, 
+    port = process.env.PORT;
