718f7ff778d8438fa13e288f27e82e4f746bab30
Finn Brandt <finn2003minicooper@gmail.com>
2022-02-17 12:37:42 +0100

    rank commands

--- src/commands/files/lookup/rankCategory.ts
+++ src/commands/files/lookup/rankCategory.ts
@@ -1,5 +1,37 @@
+import { APIEmbed } from "discord-api-types";
 import { CommandInteraction } from "discord.js";
+import guildsCfg from '../../../config/guildConfig.json';
+import { Category } from "../../../util/roleUpdate";
+import { getMembersWithPoints } from "../../../util/sheets";
 
 export async function run (interaction: CommandInteraction<"present">) {
-    interaction.deferReply();
+    const defer = interaction.deferReply();
+
+    const guildCfg = (guildsCfg as any)[interaction.guildId] ?? guildsCfg.default;
+    const tag = interaction.user.tag;
+    const patch = interaction.options.getString('patch', false) ?? '1.50';
+    const season = interaction.options.getString('season', true);
+    const category: Category = interaction.options.getString('category', true) as Category;
+
+    const memberPoints = await getMembersWithPoints(interaction.guildId, { patch, season });
+    const user = memberPoints.get(tag);
+    if (!user || user[category] === 0) {
+        const embed: APIEmbed = {
+            description: `Unable to find your season ${season} ${category} rank.`,
+            color: guildCfg.embeds.error
+        }
+        await defer;
+        interaction.editReply({ embeds: [embed] });
+        return;
+    }
+    let rank = 1;
+    for (const memberPoint of memberPoints.values())
+        if (memberPoint[category] > user[category])
+            rank++;
+    const embed: APIEmbed = {
+        title: `Category Rank:`,
+        description: `Rank: *${rank}*\nPoints: *${user[category]}*`
+    }
+    await defer;
+    interaction.editReply({ embeds: [embed] });
 }
--- src/commands/files/lookup/rankSeason.ts
+++ src/commands/files/lookup/rankSeason.ts
@@ -1,5 +1,35 @@
+import { APIEmbed } from "discord-api-types";
 import { CommandInteraction } from "discord.js";
+import { getMembersWithPoints } from "../../../util/sheets";
+import guildsCfg from '../../../config/guildConfig.json';
 
 export async function run (interaction: CommandInteraction<"present">) {
-    interaction.deferReply();
+    const defer = interaction.deferReply();
+
+    const guildCfg = (guildsCfg as any)[interaction.guildId] ?? guildsCfg.default;
+    const tag = interaction.user.tag;
+    const patch = interaction.options.getString('patch', false) ?? '1.50';
+    const season = interaction.options.getString('season', true);
+
+    const memberPoints = await getMembersWithPoints(interaction.guildId, { patch, season });
+    const user = memberPoints.get(tag);
+    if (!user) {
+        const embed: APIEmbed = {
+            description: `Unable to find your season ${season} rank.`,
+            color: guildCfg.embeds.error
+        }
+        await defer;
+        interaction.editReply({ embeds: [embed] });
+        return;
+    }
+    let rank = 1;
+    for (const memberPoint of memberPoints.values())
+        if (memberPoint.Total > user.Total)
+            rank++;
+    const embed: APIEmbed = {
+        title: `Season Rank:`,
+        description: `Rank: *${rank}*\nPoints: *${user.Total}*`
+    }
+    await defer;
+    interaction.editReply({ embeds: [embed] });
 }
