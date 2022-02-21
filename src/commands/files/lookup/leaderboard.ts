40e9b511a350fef17f1485f4f5e956e21f993bba
Finn Brandt <finn2003minicooper@gmail.com>
2022-02-21 18:37:04 +0100

    Add points and place

--- src/commands/files/lookup/leaderboard.ts
+++ src/commands/files/lookup/leaderboard.ts
@@ -1,5 +1,15 @@
 import { CommandInteraction } from "discord.js";
+import { getAllSubmits } from "../../../util/sheets";
 
 export async function run (interaction: CommandInteraction<"present">) {
     interaction.deferReply();
+
+    const patch = interaction.options.getString('patch', false) ?? '1.50';
+    const season = interaction.options.getString('season', true);
+    const category = interaction.options.getString('category', true);
+    let map = interaction.options.getString('map');
+
+
+    getAllSubmits(interaction.guildId, { patch, season });
+    
 }
---
+++ src/util/runs.ts
@@ -0,0 +1,110 @@
+import { Run } from "./sheets";
+
+/**
+ * Sorts the runs by time and if the time is equal by submit date.
+ * This methods transforms the original array and returns the transformed original array.
+ * @param runs The runs to sort.
+ * @returns The sorted runs.
+ */
+ export function sortRuns(runs: Run[]): Run[] {
+    runs.sort((run1, run2) => {
+        const timeDiff = run1.time - run2.time;
+        return timeDiff ? timeDiff : run1.date.getTime() - run2.date.getTime();
+    });
+    return runs;
+}
+
+
+/**
+ * Filters an array so that only pbs are left. Each combo of:
+ * patch, season, category, map, and user
+ * has its own pb.
+ * @param runs The runs to extract the pbs out of.
+ * @returns An array consisting only of the pbs in the runs array.
+ */
+ export function pbsOnly(runs: Run[]): Run[] {
+    const pbs: Run[] = [];
+    for (const run of runs) {
+        const index = pbs.findIndex(run1 => 
+            run1.patch === run.patch
+            && run1.season === run.season
+            && run1.category === run.category
+            && run1.map === run.map
+            && run1.username === run.username
+        );
+        if (index === -1) {
+            pbs.push(run);
+            continue;
+        }
+        if (run.time < pbs[index].time || (run.time === pbs[index].time && run.date.getTime() < pbs[index].date.getTime()))
+            pbs[index] = run;
+    }
+    return pbs;
+}
+
+
+export interface RunWithPlaceAndPoints extends Run {
+    place: number,
+    points: number
+}
+
+/**
+ * Adds place and points to the given runs. The returned runs are also sorted.
+ * Place and points are missing from the runs returned in getAllSubmits bc their calculation are quite resource intensive
+ * and should only be done if necessary.
+ * @param runs The runs the add the place and points to.
+ * @returns The runs with their respective place and points.
+ */
+export function addPlaceAndPoints(runs: Run[]): RunWithPlaceAndPoints[] {
+    sortRuns(runs);
+    const mappedRuns = runs.map<RunWithPlaceAndPoints>(run => {
+        const sameMap = runs.filter(run1 => run1.patch === run.patch 
+            && run1.season === run.season 
+            && run1.category === run.category
+            && run1.map === run.map);
+        const wr = sameMap[0];
+        let place = runs.filter(run1 => run1.time - run.time < 0.0002).length + 1;
+        if (place === 1 && run !== wr) place = 2;
+        const points = getPoints(run, wr, place, run.map, run.category);
+        return { ...run, place, points };
+    });
+    return mappedRuns;
+}
+
+
+export function getPercentilePoints(run: Run, wr: Run) {
+    let normalizedTime: number;
+    if ((normalizedTime = 1 - ((run.time - wr.time) / wr.time)) < 0) normalizedTime = 0;
+    return Math.round((0.4 * normalizedTime**25 + 0.05 * normalizedTime**4 + 0.25 * normalizedTime**3 + 0.3 * normalizedTime**2) * 100);
+}
+
+
+export function getPoints(run: Run, wr: Run, place: number, map: string, category: string) {
+    return getPercentilePoints(run, wr) + getMapBasePoints(map, category) + getPlacePoints(place);
+}
+
+
+export function getMapBasePoints(map: string, category: string) {
+    let points;
+    if (['Gibraltar', 'Havana', 'Rialto', 'Route66'].includes(map)) points = 80;
+    else if (['Lijiang Control Center', 'Hollywood', 'Eichenwalde', 'Busan MEKA Base'].includes(map)) points = 70;
+    else if (['Volskaya Industries', 'Paris', 'Numbani', 'Nepal Shrine', 'Nepal Sanctum', 'Lijiang Night Market', 'King\'s Row', 'Junktertown', 'Horizon Lunar Colony', 'Hanamura', 'Dorado'].includes(map)) points = 60;
+    else if (['Temple of Anubis', 'Oasis University', 'Oasis City Center', 'Nepal Village', 'Lijiang Garden', 'Ilios Well', 'Illios Ruins', 'Illios Lighthouse', 'Busan Sanctuary', 'Busan Downtown', 'Blizzard World'].includes(map)) points = 50;
+    else points = 40;
+    return category === 'Standard' ? points : points / 2;
+}
+
+
+export function getPlacePoints(place: number) {
+    switch(place) {
+        case 1: return 200;
+        case 2: return 180;
+        case 3: return 140;
+        case 4: return 120;
+        case 5: return 100;
+        case 6: return 60;
+        case 7: return 40;
+        case 8: return 20;
+        default: return 0;
+    }
+}
