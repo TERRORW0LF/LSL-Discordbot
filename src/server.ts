ee9484e103dbcfd3860be49c31d5c7a44b3b08bc
Finn Brandt <finn2003minicooper@gmail.com>
2022-02-23 16:44:48 +0100

    some fixes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Client, Intents } from 'discord.js';
-import { discordToken, port } from './config/config';
+import { discordToken, herokuAuth, port } from './config/config';
 import commandCollection from './commands/commandCollection';
 import interactionCreate from './events/discord/interactionCreate';
 import ready from './events/discord/ready';
@@ -10,6 +10,7 @@ import ping from './events/server/ping';
 import listen from './events/server/listen';
 import guildMemberAdd from './events/discord/guildMemberAdd';
 import guildMemberUpdate from './events/discord/guildMemberUpdate';
+import axios from 'axios';
 
 process.on('unhandledRejection', () => {});
 process.on('uncaughtException', () => {});
@@ -17,11 +18,17 @@ process.on('uncaughtException', () => {});
 // API
 const app = express();
 
+app.use(express.json());
 app.get('/ping', (...args) => ping(...args));
 app.post('/submit', (...args) => submit(client, ...args));
 app.post('/delete', (...args) => remove(client, ...args));
 
 app.listen(port, (...args) => listen(...args));
+
+setInterval(async () => {
+    axios.get(`https://lsl-discordbot-v12.herokuapp.com/ping?auth=${herokuAuth}`).catch(err => { });
+    axios.get(`https://discord-lsl.herokuapp.com/ping?auth=${herokuAuth}`).catch(err => { });
+}, 1200000);
 
 // Discord Client
 const client = new Client({ restTimeOffset: 100, partials: ['USER', 'GUILD_MEMBER', 'MESSAGE', 'REACTION'], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
