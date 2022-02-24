86f8daf3ca4dc3ff4807f80edad970ad0ae07cb7
Finn Brandt <finn2003minicooper@gmail.com>
2022-02-24 14:54:37 +0100

    runs -> pbs

--- src/commands/files/lookup/place.ts
+++ src/commands/files/lookup/place.ts
@@ -35,9 +35,9 @@ export async function run (interaction: CommandInteraction<"present">) {
     let reqPlace: number;
     if (rawPlace === 0) reqPlace = 1;
     else if (rawPlace < 0)
-        if (rawPlace + runs.length <= 0) reqPlace = 1;
+        if (rawPlace + pbs.length <= 0) reqPlace = 1;
         else reqPlace = rawPlace + pbs.length + 1;
-    else if (rawPlace > runs.length) reqPlace = pbs.length;
+    else if (rawPlace > pbs.length) reqPlace = pbs.length;
     else reqPlace = rawPlace;
     const run = pbs.at(reqPlace - 1);
     if (!run) {
@@ -53,7 +53,7 @@ export async function run (interaction: CommandInteraction<"present">) {
     const place = getPlace(run, pbs[0], pbs);
     const points = getPoints(run, pbs[0], place, map, category);
     let samePlaceUsers = "";
-    if (run.submitId !== runs[0].submitId)
+    if (run.submitId !== pbs[0].submitId)
         samePlaceUsers = pbs.filter(run1 => run1.time === run.time && run1.username !== run.username).join(', ');
     
     const embed: APIEmbed = {
