1f2f953eadb977c647070cc3c86c72991b0611df
Finn Brandt <finn2003minicooper@gmail.com>
2023-01-09 12:28:27 +0100

    Fixes

--- src/commands/files/fun/coinFlip.ts
+++ src/commands/files/fun/coinFlip.ts
@@ -3,6 +3,6 @@ import guildsCfg from '../../../config/guildConfig.json' assert { type: 'json' }
 
 export async function run(interaction: CommandInteraction<'present'>): Promise<void> {
     const guildCfg = (guildsCfg as any)[interaction.guildId] ?? guildsCfg.default;
-    const msg = `${Formatters.userMention(interaction.user.id)} flipped a coin and got **${Math.round(Math.random()) ? '**Heads**' : '**Tails**'}!`;
+    const msg = `${Formatters.userMention(interaction.user.id)} flipped a coin and got **${Math.round(Math.random()) ? 'Heads' : 'Tails'}**!`;
     await interaction.reply({ embeds: [{color: guildCfg.embeds.success, description: msg }] });
 }
